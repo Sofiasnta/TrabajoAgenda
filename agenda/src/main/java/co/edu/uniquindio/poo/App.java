d95dccf2ba3dab38b86c9b3bffb5b92d824acc06
Santiago-Bayona <Cesars.bayonap@uqvirtual.edu.co>
2024-08-31 19:10:48 -0500

    Update App.java

--- agenda/src/main/java/co/edu/uniquindio/poo/App.java
+++ agenda/src/main/java/co/edu/uniquindio/poo/App.java
@@ -1,32 +1,35 @@
-package co.edu.uniquindio.poo;
+package Agenda;
 
-/**
- * Hello world!
- *
- */
-public class App {
+public class App2 {
     public static void main(String[] args) {
         Contacto contacto1 = new Contacto("Nicolas","nico","Habitat","3057970008","nicolas@gmail.com");
-        Contacto contacto2 = new Contacto("Nicolas","nico","Habitat","3057008","nicolas@gmail.com");
+        Contacto contacto2 = new Contacto("Luis","lucho","Habitat","3057007878","Luis@gmail.com");
         Contacto contacto3 = new Contacto("Nicolas","nico","Habitat","3057970008","nicolas@gmail.com");
         Contacto contacto4 = new Contacto("Nicolas","nico","Habitat","3057970008","nicolas@gmail.com");
         Contacto contacto5 = new Contacto("Nicolas","nico","Habitat","3057970008","nicolas@gmail.com");
         Contacto contacto6 = new Contacto("Nicolas","nico","Habitat","3057970008","nicolas@gmail.com");
 
         Grupo grupoIglesia = new Grupo("Grupo Iglesia");
+
         grupoIglesia.agregarContacto(contacto1);
         grupoIglesia.agregarContacto(contacto2);
-        grupoIglesia.agregarContacto(contacto2);
+        grupoIglesia.agregarContacto(contacto3);
 
         Grupo eam = new Grupo("eam");
         eam.agregarContacto(contacto1);
 
 
-        System.out.println(grupoIglesia.getContactos());
+        System.out.println(grupoIglesia);
         System.out.println(eam.getContactos());
+
+        Reunion FinDeAño = new Reunion("Reunion de fin de año","31 de diciembre","20:00");
+        FinDeAño.agregarContacto(contacto1);
+        FinDeAño.agregarContacto(contacto6);
+
+        System.out.println(FinDeAño);
 
 
     }
 
-    
+
 }
