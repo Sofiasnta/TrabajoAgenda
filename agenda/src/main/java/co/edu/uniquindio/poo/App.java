f349c8da8768aaec4bc25bef52e865552014ab4a
Santiago-Bayona <Cesars.bayonap@uqvirtual.edu.co>
2024-09-01 16:42:58 -0500

    Update App.java

--- agenda/src/main/java/co/edu/uniquindio/poo/App.java
+++ agenda/src/main/java/co/edu/uniquindio/poo/App.java
@@ -1,59 +1,46 @@
-package co.edu.uniquindio.poo;
+package Agenda;
 
-public class App {
-    
+public class App2 {
     public static void main(String[] args) {
-        Contacto contacto1 = new Contacto("Nicolas", "nico", "Circasia", "3057970008", "nicolas@gmail.com");
-        Contacto contacto2 = new Contacto("Monica", "Moni", "Medellín", "3057008", "moni@gmail.com");
+        Contacto contacto1 = new Contacto("Nicolas","nico","Habitat","3057008","nicolas@gmail.com");
+        Contacto contacto2 = new Contacto("Luara", "lau", "Habitat", "30570474808", "laura@gmail.com");
         Contacto contacto3 = new Contacto("Sofia", "Sofi", "Armenia", "3123819703", "sofisanta@gmail.com");
         Contacto contacto4 = new Contacto("Santiago", "Santi", "Pereira", "3102891032", "santiago@gmail.com");
         Contacto contacto5 = new Contacto("Andrés", "Andi", "Circasia", "3053471208", "andres@gmail.com");
         Contacto contacto6 = new Contacto("Ana", "Anita", "Calarca", "3117972345", "anita@gmail.com");
+        Contacto contacto7 = new Contacto("Nicolas","nico","Habitat","3057008","nicolas@gmail.com");
+
 
         Grupo grupoIglesia = new Grupo("Grupo Iglesia", Grupo.Categoria.Iglesia);
 
-        System.out.println(grupoIglesia.getNombre());
-        grupoIglesia.setGrupo("Reunion Jovenes SABADOS");
-
         grupoIglesia.agregarContacto(contacto1);
         grupoIglesia.agregarContacto(contacto2);
-        grupoIglesia.agregarContacto(contacto2);
+        grupoIglesia.agregarContacto(contacto3);
+        grupoIglesia.agregarContacto(contacto4);
+        grupoIglesia.agregarContacto(contacto5);
+        //grupoIglesia.agregarContacto(contacto6);
 
-        System.out.println(grupoIglesia.getContactos());
 
-        Grupo universidad = new Grupo("Universidad Trabajo", Grupo.Categoria.Oficina);
+
+
+        Grupo universidad = new Grupo("Universidad", Grupo.Categoria.Trabajo);
+        universidad.agregarContacto(contacto1);
         universidad.agregarContacto(contacto3);
+       // universidad.agregarContacto(contacto7);
 
-        System.out.println(universidad.getNombre());
-        System.out.println(universidad.getContactos());
 
-        System.out.println(grupoIglesia.getNombre());
-        grupoIglesia.setGrupo("Iglesia Domingo");
-        System.out.println(grupoIglesia.getNombre());
 
-        Reunion finDeAnio = new Reunion("Fin de año", "31 enero", "20:00");
-        finDeAnio.agregarContacto(contacto1);
-        finDeAnio.agregarContacto(contacto6);
+        System.out.println(grupoIglesia);
+        System.out.println(universidad);
 
-        System.out.println(finDeAnio);
+        Reunion FinDeAño = new Reunion("Reunion de fin de año","31 de diciembre","20:00");
+        FinDeAño.agregarContacto(contacto1);
+        FinDeAño.agregarContacto(contacto6);
+        FinDeAño.eliminarContacto("Nicolas","3057008");
 
-        Grupo fiesta = new Grupo ("fiesta Sabado", Grupo.Categoria.Fiesta);
+        System.out.println(FinDeAño);
 
-        System.out.println(fiesta.getNombre());
-
-        fiesta.agregarContacto(contacto6);
-        fiesta.agregarContacto(contacto1);
-        fiesta.agregarContacto(contacto5);
-        fiesta.agregarContacto(contacto4);
-        fiesta.agregarContacto(contacto3);
-        fiesta.agregarContacto(contacto2);
-         
-        System.out.println(fiesta.getContactos());
-
-        contacto1.setNombre("Rosa");
-        System.out.println(contacto1);
-
+        contacto2.setNombre("Rosa");
+        System.out.println(contacto2);
     }
-
 }
-
