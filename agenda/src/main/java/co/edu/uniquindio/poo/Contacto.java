a8916573d756697320a980570af8a70ba876bc13
Santiago-Bayona <Cesars.bayonap@uqvirtual.edu.co>
2024-09-01 16:45:20 -0500

    Update Contacto.java

--- agenda/src/main/java/co/edu/uniquindio/poo/Contacto.java
+++ agenda/src/main/java/co/edu/uniquindio/poo/Contacto.java
@@ -1,52 +1,62 @@
-package co.edu.uniquindio.poo;
-
+package Agenda;
 
 public class Contacto {
-    private String nombre;
-    private String alias;
-    public String direccion;
-    public String telefono;
-    public String email;
+    public String Nombre;
+    public String Alias;
+    public String Direccion;
+    public String Telefono;
+    public String Email;
 
-    public Contacto(String nombre, String alias, String direccion, String telefono, String email) {
-        this.nombre = nombre;
-        this.alias = alias;
-        this.direccion = direccion;
-        this.telefono = telefono;
-        this.email = email;
+    public Contacto(String Nombre,String Alias, String Direccion, String Telefono, String Email){
+        this.Nombre=Nombre;
+        this.Alias=Alias;
+        this.Direccion=Direccion;
+        this.Telefono=Telefono;
+        this.Email=Email;
     }
 
+    public String getNombre(){
+        return Nombre;
+    }
+    public void setNombre(String Nombre){
+        this.Nombre=Nombre;
+    }
 
-    public String getNombre() {
-        return nombre;
+    public String getAlias(){
+        return Alias;
     }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setAlias(String Alias){
+        this.Alias=Alias;
     }
-    public String getAlias() {
-        return alias;
-    }
-    public void setAlias(String alias) {
-        this.alias = alias;
-    }
-    public String getDireccion() {
-        return direccion;
-    }
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-    public String getTelefono() {
-        return telefono;
-    }
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }   
 
+    public String getDireccion(){
+        return Direccion;
+    }
+    public void setDireccion(String Direccion){
+        this.Direccion=Direccion;
+    }
 
+    public String getTelefono(){
+        return Telefono;
+    }
+    public void setTelefono(String Telefono){
+        this.Telefono=Telefono;
+    }
+
+    public String getEmail(){
+        return Email;
+    }
+    public void setEmail(String Email){
+        this.Email=Email;
+    }
+    @Override
+    public String toString() {
+        return "Contacto{" +
+                "Nombre='" + Nombre + '\'' +
+                ", Alias='" + Alias + '\'' +
+                ", Direccion='" + Direccion + '\'' +
+                ", Telefono='" + Telefono + '\'' +
+                ", Email='" + Email + '\'' +
+                '}';
+    }
 }
